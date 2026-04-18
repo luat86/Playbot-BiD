743e44b0cdad4b92d388f89d36ccffa0e0f1e589
NGUYỄN VĂN LUẬT <Luatgm@gmail.com>
2026-04-18 12:19:56 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Đảm bảo đường dẫn luôn đúng trên GitHub Pages
+  base: '/Playbot-BiD/', // Phải chính xác là tên repository của bạn trên GitHub
 })
