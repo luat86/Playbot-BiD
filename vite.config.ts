c7d493ef0d57541cab5131e0ec48b8e8ad7a4056
NGUYỄN VĂN LUẬT <Luatgm@gmail.com>
2026-04-18 12:40:14 +0700

    Fix formatting in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Cấu hình chuẩn cho GitHub Pages
 export default defineConfig({
   plugins: [react()],
-  base: '/Playbot-BiD/',
+  base: '/Playbot-BiD/', 
 })
